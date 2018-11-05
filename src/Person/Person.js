6b3c08ec733b6cdaeb0d62101e2bc0a0c9f18965
Hari <neyanihari@gmail.com>
2018-11-06 00:11:49 +0530

    Updated the Package and Added the class Files

---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm a {props.name} Being with {props.age}</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}/>
+        </div>
+    )
+}
+
+export default person;
